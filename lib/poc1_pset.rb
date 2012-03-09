21a9c959334aaae7e9ad9053c689fa7048898f8d
Greg Belote <etoleb@gmail.com>
2012-03-09 01:23:32 -0500

    tweaks and hacking

--- lib/poc1_pset.rb
+++ lib/poc1_pset.rb
@@ -1,5 +1,8 @@
 require "poc1_pset/version"
 
+# Include the problems here...
+require "poc1_pset/problem1"
+
 module Poc1Pset
   # Your code goes here...
 end
---
+++ lib/poc1_pset/problem1.rb
@@ -0,0 +1,10 @@
+
+require 'lambdalib/pset'
+
+module Poc1Pset
+  class Problem1 < Lambdalib::Pset
+    short_name "Problem 1"
+    description "Create a model `Comment` that includes three attributes: `body`, `author`, and `website`."
+  end
+end
+
