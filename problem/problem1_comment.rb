98873737012c21eaf069773701569f98b7838012
Greg Belote <etoleb@gmail.com>
2012-03-09 17:35:07 -0500

    adds stuff for associations

--- problem/problem1_comment.rb
+++ problem/problem1_comment.rb
@@ -21,7 +21,7 @@ end
 attrs = { }
 Comment.columns.each { |c| attrs[ c.name ] = c.type }
 
-{ "body" => :text, "author" => :string, "website" => :string }.each do |key, val|
+{ "body" => :text, "author" => :string, "website" => :string, "post_id" => :integer }.each do |key, val|
   unless attrs.has_key? key
     puts "#{ '✗'.red } #{ 'Comment'.cyan } missing #{ key.bold } attribute."
     exit 1
@@ -33,7 +33,17 @@ Comment.columns.each { |c| attrs[ c.name ] = c.type }
   end
 end
 
+unless Comment.reflect_on_association(:post).try(:macro) == :belongs_to
+  puts "#{ '✗'.red } #{ "Comment".cyan } should #{ 'belong to'.bold } a #{ 'Post'.cyan } object."
+  exit 1
+end
 
+unless Post.reflect_on_association(:comments).try(:macro) == :has_many
+  puts "#{ '✗'.red } #{ "Post".cyan } should #{ 'have many'.bold } a #{ 'Comment'.cyan } objects."
+  exit 1
+end
+
+puts "#{ '✓'.green } Associated Comments with Posts"
 
 puts "#{ '✓'.green } Defined body, author, and website attributes for Comment."
 
