dbce819bebc20166abf913c1ae113d6bb2ce9480
Greg Belote <etoleb@gmail.com>
2012-03-22 09:28:25 -0400

    tweak

--- lib/poc1_pset/problem/up_and_running.rb
+++ lib/poc1_pset/problem/up_and_running.rb
@@ -5,7 +5,7 @@ module Poc1Pset
     class UpAndRunning < Lambdalib::Pset::Problem
       short_name "Up and Running"
 
-      add_test :bundle
+      #add_test :bundle
       add_test :rails_s
     end
   end
